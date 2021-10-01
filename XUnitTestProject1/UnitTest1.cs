d56706b0ae3368226790cff4e1d1e3f9dbc26696
Kåre Mølgaard Rasmussen <34368915+dk-moelgaard75@users.noreply.github.com>
2021-10-02 00:22:49 +0200

    Initial commit

---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using sl_test_2021_uge4;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            int a = 1;
+            int b = 2;
+            int expected = 3;
+
+            //Act
+            Calculator calc = new Calculator();
+            int result = calc.add(a, b);
+            //assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ sl_test_2021_uge4/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sl_test_2021_uge4
+{
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int multi(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
